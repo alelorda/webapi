61feb28c985a336e78d38ef3c7da5a0b64a5296c
usuariosala <usuariosala@ITESLABPC05.ites-sr.local>
2021-09-24 22:55:44 -0300

    first commit

---
+++ Model/Persona.cs
@@ -0,0 +1,11 @@
+namespace apiDemo.Models{
+    public class Persona{
+        public int id {get;set;}
+        public string nombre {get;set;}
+        public string mail {get;set;}
+        public string domicilio {get;set;}
+        public string telefono {get;set;}
+        public string foto {get;set;}
+        public bool activo {get;set;}
+    }
+}
