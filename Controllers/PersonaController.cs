a72b3e16d11529ac084c2c59039533408eb78bc0
usuariosala <usuariosala@ITESLABPC05.ites-sr.local>
2021-09-24 23:02:38 -0300

    first commit

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -13,6 +13,19 @@ namespace apiDemo.Controllers
     [Route("api/[controller]")]
     public class PersonaController : controllerBase
     {
+        private readonly List<Persona> persona = new List<Persona>();
+
+        public PersonaController(){
+            persona.Add(new Persona(){
+                id=1,
+                Nombre = "Luis Garcia",
+                Email = "lusito"
+                Domicio = "sarasa"
+                Foto = "asadasd",
+                Activo = true,
+            }
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<Persona>> Get(){
             return OK(Persona);
